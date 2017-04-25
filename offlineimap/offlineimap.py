d2526dd878c30c79c1a4e7146d564b4043e20b03
Max Franke <max@mumintroll.org>
2017-04-25 09:07:27 +0200

    Added studimail account to offlineimap
    Make offlineimap also create the mailboxes command for mutt to use.

--- offlineimap/offlineimap.py
+++ offlineimap/offlineimap.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
 from subprocess import check_output
 
-def get_pass():
-    return check_output("pass sniff@mumintroll.org", shell=True).splitlines()[0]
+def get_pass(for_="sniff@mumintroll.org"):
+    return check_output("pass {}".format(for_), shell=True).splitlines()[0]
