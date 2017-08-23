1ddedf756225a5998058cab447c8cd29d3dfa8d5
Max Franke <max@mumintroll.org>
2017-08-23 16:04:44 +0200

    Added function to print all colors

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -12,3 +12,16 @@ any() {
         ps xauwww | grep -i --color=auto "[${1[1]}]${1[2,-1]}"
     fi
 }
+
+# -------------------------------------------------------------------
+# Show terminal colors
+# -------------------------------------------------------------------
+show-colors() {
+for line in {0..17}; do
+    for col in {0..15}; do
+        code=$(( $col * 18 + $line ));
+        printf $'\e[38;05;%dm %03d' $code $code;
+    done;
+    echo;
+done
+}
