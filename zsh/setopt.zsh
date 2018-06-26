0171f901eec9e5a1d5c4588456c67b4a0e530d27
Max Franke <max@mumintroll.org>
2018-06-26 10:05:10 +0200

    [zsh] Do not verify history expansion (bang syntax)

--- zsh/setopt.zsh
+++ zsh/setopt.zsh
@@ -18,7 +18,6 @@ setopt hist_ignore_dups # Do not write events to history that are duplicates of 
 setopt hist_ignore_space # remove command line from history list when first character on the line is a space
 setopt hist_find_no_dups # When searching history don't display results already cycled through twice
 setopt hist_reduce_blanks # Remove extra blanks from each command line being added to history
-setopt hist_verify # don't execute, just expand history
 setopt share_history # imports new commands and appends typed commands to history
 
 # ===== Completion
