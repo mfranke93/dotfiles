92a937d4b0fc78f5532a3366e2d17b733b3bebba
Max Franke <max@mumintroll.org>
2018-08-25 11:58:25 +0200

    [mutt] Fixed inbox tag hook

--- notmuch-hook.sh
+++ notmuch-hook.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 notmuch new
 # tag all messages from "me" as sent and remove tags inbox and unread
-notmuch tag -new -inbox +sent -- from:sniff@mumintroll.org or from:max@mumintroll.org
+notmuch tag -new -inbox +sent -- 'from:"/(max|sniff)@mumintroll.org/"'
